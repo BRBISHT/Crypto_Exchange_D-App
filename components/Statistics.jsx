cae13d61a999c6bf4293df71490e1de7b3cffd72
BRIJENDRA S BISHT <brijendrabisht1999@gmail.com>
2024-05-18 22:49:49 +0530

    modified:   components/Feature.jsx 	modified:   components/Footer.jsx 	modified:   components/Header.jsx 	modified:   components/Hero.jsx 	modified:   components/Platfrom.jsx 	modified:   components/Preloader.jsx 	modified:   components/Scurity.jsx 	modified:   components/Statistics.jsx 	modified:   components/Testomonial.jsx 	modified:   components/Token.jsx 	modified:   context/constants.js 	modified:   context/context.js 	new file:   fork/abis__erc20.json 	new file:   fork/hardhat.config.js 	new file:   fork/package-lock.json 	new file:   fork/package.json 	new file:   fork/scripts/deploy.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/_app.js 	modified:   pages/index.js 	modified:   utils/index.js

--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -1,7 +1,178 @@
 import React from "react";
 
 const Statistics = () => {
-  return <div>Statistics</div>;
+  return (
+    <div className="statistic">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-sm-6
+        wow fadeInUp"
+            data-wow-duration="0.3s"
+            data-wow-delay="0.3s"
+          >
+            <div className="platfrom-box text-center">
+              <div className="icon">
+                <img src="assets/img/offer-icon-1.png" alt="" />
+              </div>
+              <p className="text">
+                Extra fast <br />Transaction
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-sm-6 wow
+        fadeInUp
+        "
+            data-wow-duration="0.4s"
+            data-wow-delay="0.4s"
+          >
+            <div className="platfrom-box text-center">
+              <div className="icon">
+                <img src="assets/img/offer-icon-2.png" alt="" />
+              </div>
+              <p className="text">
+                Extra fast <br />Transaction
+              </p>
+            </div>
+
+
+          </div>
+          <div className="col-lg-3 col-sm-6 wow
+        fadeInUp
+        "
+            data-wow-duration="0.5s"
+            data-wow-delay="0.5s"
+          >
+            <div className="platfrom-box text-center">
+              <div className="icon">
+                <img src="assets/img/offer-icon-3.png" alt="" />
+              </div>
+              <p className="text">
+                No Limits On <br />Exchange
+              </p>
+            </div>
+
+
+          </div>
+
+          <div className="col-lg-3 col-sm-6 wow
+        fadeInUp
+        "
+            data-wow-duration="0.6s"
+            data-wow-delay="0.6s"
+          >
+            <div className="platfrom-box text-center">
+              <div className="icon">
+                <img src="assets/img/offer-icon-4.png" alt="" />
+              </div>
+              <p className="text">
+                We have best <br />Exchange rate
+              </p>
+            </div>
+
+
+          </div>
+
+          <div className="col-12">
+            <div className="content">
+              <div className="bg-pic">
+                <img src="assets/img/stasictic-bg.png" alt="" />
+              </div>
+              <div className="section-head text-center
+              wow fadeInUp
+              "
+                data-wow-duration="0.3s"
+                data-wow-delay="0.3s"
+              >
+                <h4 className="lasthead">
+                  Our stats say more than words
+                </h4>
+                <h2 className="title">
+                  Today's Statistics
+                </h2>
+                <p className="text">
+                  Lorem ipsum dolor sit, amet consectetur
+                  adipisicing elit. Excepturi architecto
+                  incidunt, doloribus voluptate asperiores quam.
+                  Tempora nihil cupiditate odit earum dolorem!
+
+                  Consequatur nemo voluptates cupiditate.
+                </p>
+              </div>
+
+              <div className="row justify-content-center">
+                <div className="col-xl-4 col-lg-6 wow
+                fadeInUp
+                "
+                  data-wow-duration="0.3s"
+                  data-wow-delay="0.3s"
+
+                >
+
+                  <div className="statis-boxx">
+                    <div className="icon">
+
+                      <img src="assets/img/stasictic-icon-1.png" alt="" />
+                    </div>
+
+                    <div className="statis-content">
+
+                      <h3 className="suntitle">483</h3>
+                      <p className="text">
+                        Transaction Made
+                      </p>
+                    </div>
+                  </div>
+
+
+                </div>
+                <div className="col-xl-4 col-lg-6 wow
+                fadeInUp
+                "
+                  data-wow-duration="0.3s"
+                  data-wow-delay="0.3s"
+
+                >
+
+                  <div className="statis-boxx">
+                    <div className="icon">
+
+                      <img src="assets/img/stasictic-icon-1.png" alt="" />
+                    </div>
+
+                    <div className="statis-content">
+
+                      <h3 className="suntitle">ETH-BCH</h3>
+                      <p className="text">
+                        Today's Champion pair
+                      </p>
+                    </div>
+                  </div>
+                  
+                </div>
+                <div className="col-xl-4 col-lg-6 wowfadeInUp"
+                    data-wow-duration="0.5s"
+                    data-wow-delay="0.5s"
+                  >
+                    <div className="statis-boxx">
+                      <div className="icon">
+                        <img src="assets/img/stasictic-icon-1.png" alt="" />
+                      </div>
+                      <div className="statis-content">
+                        <h3 className="suntitle">12 Mins</h3>
+                        <p className="text">
+                          Average processing time
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 
+import { PROVIDER } from "../context/context";
+import toast, { Toaster } from "react-hot-toast";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <PROVIDER>
+        <Component {...pageProps} />
+      </PROVIDER>
+      <Toaster/>
 
       <script src="assets/js/jquery.min.js"></script>
       <script src="assets/js/proper-min.js"></script>
